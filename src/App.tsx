61668074962807efcf6b96313a95f1aa0106f893
Geeshr <geeshni1@gmail.com>
2021-11-06 17:07:54 -0400

    added clouds

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,13 @@ const App = () => {
           <TodoBar />
         </div>
       </div>
-
+      <div id="clouds">
+        <div className="cloud x1"></div>
+        <div className="cloud x2"></div>
+        <div className="cloud x3"></div>
+        <div className="cloud x4"></div>
+        <div className="cloud x5"></div>
+      </div>
     </>
   );
 };
